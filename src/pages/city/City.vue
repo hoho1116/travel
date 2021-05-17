1a246e4bf559908f8a29d4720c5f66920a4a0ca4
Ho <1105799593@qq.com>
2021-05-17 16:03:59 +0800

    city router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,18 @@
+<template>
+    <city-header/>
+</template>
+
+<script>
+import CityHeader from '../city/components/Header'
+
+export default {
+    name:'City',
+    components: { 
+        CityHeader
+     }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../pages/home/Home.vue'
+import City from '../pages/city/City.vue'
 
 
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/city',
+      name: 'City',
+      component: City
     }
   ]
 })
